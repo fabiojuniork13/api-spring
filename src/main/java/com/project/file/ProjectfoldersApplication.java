8a9ab1bb4a429fa0c14d21e696cf42c01ec21ece
fabiojuniork13 <fabio_juniork13@hotmail.com>
2022-08-11 10:53:45 -0300

    Correção Nome Inicialização

---
+++ src/main/java/com/project/file/ProjectfoldersApplication.java
@@ -0,0 +1,12 @@
+package com.project.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectfoldersApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectfoldersApplication.class, args);
+	}
+
+}
